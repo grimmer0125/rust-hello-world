fdff9242b0222ae4cb0e0eaf56792330eaa323fc
grimmer <grimmer0125@gmail.com>
2019-05-21 22:42:17 +0800

    Second test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut guess = String::new();
+    let x = 5;
+    let y = 10;
+
+    println!("x = {} and y = {}", x, y);
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
