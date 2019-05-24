90f9818c4d901b2c55bba3fa83e81ae551e920ca
grimmer <grimmer0125@gmail.com>
2019-05-25 00:17:30 +0800

    Add serde json parse example

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,53 @@
+use serde_json; //::{Result, Value};
+
+use std::collections::HashMap;
 use std::io;
 
+use serde::{Deserialize, Deserializer};
+
+#[derive(Deserialize)]
+struct Person {
+    name: Option<String>, //String,
+    age: Option<u8>,      // phones: Vec<String>
+}
+
+// let data = r#"
+//     {
+//         "name": "John Doe",
+//         "age": 43,
+//         "phones": [
+//             "+44 1234567",
+//             "+44 2345678"
+//         ]
+//     }"#;
+fn serde_typed_custom_example() -> serde_json::Result<()> {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {
+            "name": "John Doe"
+        }"#;
+
+    let p: Person = serde_json::from_str(data)?;
+
+    // Do things just like with any other Rust data structure.
+    println!("person name: {:?}, age:{:?} ", p.name, p.age);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("start");
 
-    let mut guess = String::new();
-    let x = 5;
-    let y = 10;
+    serde_typed_custom_example();
 
-    println!("x = {} and y = {}", x, y);
-    // io::stdin()
-    //     .read_line(&mut guess)
-    //     .expect("Failed to read line");
+    // match untyped_example() {
+    //     Ok(_) => {
+    //         println!("one");
+    //     }
+    //     Err(error) => {
+    //         println!("The error: {}", error);
+    //     }
+    // }
 
-    println!("You guessed: {}", guess);
+    println!("end");
 }
